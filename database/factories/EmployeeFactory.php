d32e52695aede95db66c12e6f0784fa38d535828
fachriansyahmni <fachriansyahmni@gmail.com>
2022-10-05 16:11:16 +0700

    add employee and attendance factory

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employee>
+ */
+class EmployeeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'firstName' => $this->faker->firstName,
+            'lastName' => $this->faker->lastName,
+            'phone' => $this->faker->phoneNumber,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => bcrypt('password'),
+            'photo' => 'assets/emp_img.png',
+            'gender' => $this->faker->randomElement(['man', 'woman']),
+            'birthDate' => $this->faker->date(),
+            'address' => $this->faker->address,
+            'city' => $this->faker->city,
+            'nation' => $this->faker->country,
+            'roleId' => 2,
+            'isActive' => 1,
+            'joinedAt' => $this->faker->date(),
+            'statusHireId' => 1,
+        ];
+    }
+}
--- database/seeders/AttendanceSeeder.php
+++ database/seeders/AttendanceSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Attendance;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -15,31 +16,32 @@ class AttendanceSeeder extends Seeder
      */
     public function run()
     {
-        $attendance = [
-            [
-                'attendId' => 1, 
-                'employeeId' => 1, 
-                'attendanceStatusId' => 1, 
-                'submitedAt' => now(),
-                'submitedById' => 1,
-                'typeInOut' => 'In', 
-                'timeAttend' => now(), 
-                'created_at' => now(), 
-                'updated_at' => now()
-            ],
-            [
-                'attendId' => 2, 
-                'employeeId' => 1, 
-                'attendanceStatusId' => 1, 
-                'submitedAt' => now(),
-                'submitedById' => 1,
-                'typeInOut' => 'Out', 
-                'timeAttend' => now(), 
-                'created_at' => now(), 
-                'updated_at' => now()
-            ]
-        ];
+        // $attendance = [
+        //     [
+        //         'attendId' => 1, 
+        //         'employeeId' => 1, 
+        //         'attendanceStatusId' => 1, 
+        //         'submitedAt' => now(),
+        //         'submitedById' => 1,
+        //         'typeInOut' => 'In', 
+        //         'timeAttend' => now(), 
+        //         'created_at' => now(), 
+        //         'updated_at' => now()
+        //     ],
+        //     [
+        //         'attendId' => 2, 
+        //         'employeeId' => 1, 
+        //         'attendanceStatusId' => 1, 
+        //         'submitedAt' => now(),
+        //         'submitedById' => 1,
+        //         'typeInOut' => 'Out', 
+        //         'timeAttend' => now(), 
+        //         'created_at' => now(), 
+        //         'updated_at' => now()
+        //     ]
+        // ];
 
-        DB::table('attendances')->insert($attendance);
+        // DB::table('attendances')->insert($attendance);
+        Attendance::factory()->count(100)->create();
     }
 }
